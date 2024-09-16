f5365e7398c59ea342c245b1037999b4d8d8186d
Phurich Thawornying <phurich.twy@gmail.com>
2024-09-17 01:00:43 +0700

    i forgot how to CSS bruh

---
+++ 001-profile-card/app/page.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import img1 from "./img/001.png"
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="box-border h-auto w-60 bg-cyan-400 grid align-center">
+        <Image
+          className=""
+          src={img1}
+          alt="still sleep"
+          width={231}
+          height={151}
+          priority
+        />
+        <div className="">Phurich Thawornying</div>
+        <div className="">@shigupoi</div>
+        <div className="">who let her cook</div>
+        <div className="">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+        </a>
+      </div>
+      </div>
+    </main>
+  );
+}
